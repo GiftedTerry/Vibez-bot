585d7a805dde1c3f894a6baa066fe115b141c99d
GiftedTerry <protechackers@gmail.com>
2024-05-20 13:38:13 +0100

    Vibez-MD😎

---
+++ Plugins/alive.js
@@ -0,0 +1,114 @@
+const mongoose = require('mongoose');
+
+const options = {
+  temp: {
+    type: Object,
+    default: {}
+  },
+  rent: {
+    type: Object,
+    default: {}
+  }
+};
+
+const AliveSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    unique: true,
+    required: true,
+    default: 'Dockerfile'
+  },
+  alive_text: {
+    type: String,
+    default: 'You didn\'t set alive message yet\nType [.alive info] to get alive message information'
+  },
+  alive_get: {
+    type: String,
+    default: 'yes bruh?'
+  },
+  alive_url: {
+    type: String,
+    default: ''
+  },
+  alive_image: {
+    type: Boolean,
+    default: false
+  },
+  alive_video: {
+    type: Boolean,
+    default: false
+  },
+  antiviewonce: {
+    type: String,
+    default: 'true'
+  },
+  antidelete: {
+    type: String,
+    default: 'true'
+  },
+  autobio: {
+    type: String,
+    default: 'true'
+  },
+  levelup: {
+    type: String,
+    default: 'true'
+  },
+  anticall: {
+    type: String,
+    default: 'false'
+  },
+  autoreaction: {
+    type: String,
+    default: 'true'
+  },
+  permit: {
+    type: Boolean,
+    default: false
+  },
+  permit_values: {
+    type: String,
+    default: 'all'
+  },
+  chatbot: {
+    type: String,
+    default: 'true'
+  },
+  bgm: {
+    type: Boolean,
+    default: false
+  },
+  bgmarray: {
+    type: Object,
+    default: {}
+  },
+  plugins: {
+    type: Object,
+    default: {}
+  },
+  notes: {
+    type: Object,
+    default: {}
+  },
+  mention: {
+    type: Object,
+    default: {}
+  },
+  filter: {
+    type: Object,
+    default: {
+      suhail_: 'true'
+    }
+  },
+  afk: {
+    type: Object,
+    default: {}
+  },
+  ...options
+});
+
+const Alive = mongoose.model('Alive', AliveSchema);
+
+module.exports = {
+  alive: Alive
+};
