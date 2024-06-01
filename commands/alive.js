6e8c580b9d06a2a2e3a1abd41c61b86aefbcf951
GiftedTerry <protechackers@gmail.com>
2024-06-01 15:35:05 +0100

    Vibez-MD😎

--- commands/alive.js
+++ commands/alive.js
@@ -1,114 +1,15 @@
-const mongoose = require('mongoose');
+// commands/alive.js
 
-const options = {
-  temp: {
-    type: Object,
-    default: {}
-  },
-  rent: {
-    type: Object,
-    default: {}
-  }
-};
-
-const AliveSchema = new mongoose.Schema({
-  id: {
-    type: String,
-    unique: true,
-    required: true,
-    default: 'Vibez_MD'
-  },
-  alive_text: {
-    type: String,
-    default: 'You didn\'t set alive message yet\nType [.alive info] to get alive message information'
-  },
-  alive_get: {
-    type: String,
-    default: 'yes bruh?'
-  },
-  alive_url: {
-    type: String,
-    default: ''
-  },
-  alive_image: {
-    type: Boolean,
-    default: false
-  },
-  alive_video: {
-    type: Boolean,
-    default: false
-  },
-  antiviewonce: {
-    type: String,
-    default: 'true'
-  },
-  antidelete: {
-    type: String,
-    default: 'true'
-  },
-  autobio: {
-    type: String,
-    default: 'true'
-  },
-  levelup: {
-    type: String,
-    default: 'true'
-  },
-  anticall: {
-    type: String,
-    default: 'false'
-  },
-  autoreaction: {
-    type: String,
-    default: 'true'
-  },
-  permit: {
-    type: Boolean,
-    default: false
-  },
-  permit_values: {
-    type: String,
-    default: 'all'
-  },
-  chatbot: {
-    type: String,
-    default: 'true'
-  },
-  bgm: {
-    type: Boolean,
-    default: false
-  },
-  bgmarray: {
-    type: Object,
-    default: {}
-  },
-  plugins: {
-    type: Object,
-    default: {}
-  },
-  notes: {
-    type: Object,
-    default: {}
-  },
-  mention: {
-    type: Object,
-    default: {}
-  },
-  filter: {
-    type: Object,
-    default: {
-      vibez_: 'true'
-    }
-  },
-  afk: {
-    type: Object,
-    default: {}
-  },
-  ...options
-});
-
-const Alive = mongoose.model('Alive', AliveSchema);
+const { prefix } = require('../config');
+const { reply } = require('../functions');
 
 module.exports = {
-  alive: Alive
+  name: 'alive',
+  description: 'Check if the bot is alive',
+  execute(message) {
+    if (message.content === `${prefix}alive`) {
+      message.react('⚡');
+      reply(message, 'Sup boss, Vibez is alive!', 50);
+    }
+  }
 };
