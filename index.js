e37c36b9911b2a04f11bf0227adf6b1c026294a5
GiftedTerry <protechackers@gmail.com>
2024-06-10 23:54:09 +0100

    Vibez-MD😎

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
-const { Pool } = require('pg')
-
+const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-})
-
-const bot = require(__dirname + '/lib/vibez')
-const { VERSION } = require(__dirname + '/config')
+  ssl: { rejectUnauthorized: false },
+});
+const bot = require(__dirname + '/lib/vibez');
+const { VERSION } = require(__dirname + '/config');
 
 const start = async () => {
-    Debug.info(`Vibez ${VERSION}`)
   try {
-    await bot.init()
-    bot.logger.info('⏳ Database syncing!')
-    await bot.DATABASE.sync()
-    await bot.connect()
+    await bot.init();
+    bot.logger.info('⏳ Database syncing!');
+    await bot.DATABASE.sync();
+    await bot.connect();
   } catch (error) {
-    Debug.error(error);
+    console.error(error);
     start();
   }
-}
+};
+
 start();
 
-.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { results: (result) ? result.rows : null };
+    res.render('pages/db', results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
