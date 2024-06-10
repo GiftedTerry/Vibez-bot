23d29c7c619a3e1492c35c9e2e0d51cb06ad27da
GiftedTerry <protechackers@gmail.com>
2024-06-10 23:55:18 +0100

    Vibez-MD😎

---
+++ routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { results: (result) ? result.rows : null };
+    res.render('pages/db', results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
